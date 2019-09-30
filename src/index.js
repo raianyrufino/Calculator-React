11a5416a6b55194d1a3d18c2b2528c0dbda47b53
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-09-30 10:46:20 -0300

    add display

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import calculator from './main/Calculator';
+import Calculator from './main/Calculator';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
