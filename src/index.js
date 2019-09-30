2ed82d68f3ae5383ce466d612bf93832b830b7b8
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-09-30 10:53:29 -0300

    css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Calculator from './main/Calculator';
-//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
